4324a2f8e258d6c72e578b6c0a1c2d208a73da18
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-08 18:23:13 +0300

    Add level one

--- js/funnel/funnel.js
+++ js/funnel/funnel.js
@@ -10,12 +10,32 @@ xBtn.addEventListener('click', (e) => {
 
 // Function to hide WELCOME module on click
 const okidokes = document.getElementById('okidokes');
+const okidokesTwo = document.getElementById('okidokesTwo');
 const welcome = document.querySelector('.welcome');
+const levelOne = document.getElementById('levelOne');
 
 okidokes.addEventListener('click', (e) => {
 	// Hide welcome
 	e.preventDefault();
 	setTimeout(function () {
 		welcome.style.display = 'none';
-	}, 1000);
+	}, 400);
+});
+
+okidokesTwo.addEventListener('click', (e) => {
+	// Hide level one
+	e.preventDefault();
+	setTimeout(function () {
+		levelOne.style.display = 'none';
+	}, 400);
+});
+
+// Function to detect number of clicks
+var clickCounter = 0;
+window.addEventListener('click', function () {
+	clickCounter++;
+	if (clickCounter == 7) {
+		//Open it
+		levelOne.style.display = 'block';
+	}
 });
