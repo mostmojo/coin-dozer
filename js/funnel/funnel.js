1609a6411eb0ad2e6776e884a3ddbd865f979bc8
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-04-08 16:05:58 +0300

    Create welcome banner

---
+++ js/funnel/funnel.js
@@ -0,0 +1,19 @@
+// Function to hide CHATBOT on click of X button
+const xBtn = document.getElementById('xBtn');
+const chatBox = document.getElementById('chatBox');
+
+xBtn.addEventListener('click', (e) => {
+	// Hide chatbox
+	e.preventDefault();
+	chatBox.style.display = 'none';
+});
+
+// Function to hide WELCOME module on click
+const okidokes = document.getElementById('okidokes');
+const welcome = document.querySelector('.welcome');
+
+okidokes.addEventListener('click', (e) => {
+	// Hide welcome
+	e.preventDefault();
+	welcome.style.display = 'none';
+});
